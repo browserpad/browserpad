a6ca27fa4d0324587ba036b60589ee4de273dc7a
Linux in a Bit <105567407+RageGamerBoi@users.noreply.github.com>
2023-11-14 08:10:16 -0600

    Update script.js to fix 80 column bug

--- script.js
+++ script.js
@@ -59,11 +59,9 @@ document.querySelector('#open input').onchange = function () {
 //Toggle Fullwidth vs 80 Column
 document.querySelector('#fullwidth').onchange = function () {
     if (this.checked == true) {
-        document.getElementById('linewidth-guide').style.width = "calc(80ch + 1px)";
-        document.getElementById('linewidth-guide').style.width = "padding-right: calc(100% - 80ch - 0.5em - 17px)";
+        document.getElementById('textbox').style.width = "calc(80ch + 1em)";
     } else {
-        document.getElementById('linewidth-guide').style.width = "100%";
-        document.getElementById('linewidth-guide').style.width = "padding-right: 0.5em";
+        document.getElementById('textbox').style.width = "100%";
     }
 };
 
